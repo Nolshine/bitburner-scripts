6328465ac840cc5f94c41f9eceaaa6e6a4a49544
Mattar Ariel Sha'ked <mtr131@gmail.com>
2021-02-20 14:31:20 +0000

    add tools for server metrics

---
+++ metrics.script.js
@@ -0,0 +1,6 @@
+var target = args[0];
+
+tprint(getServerMinSecurityLevel(target));
+tprint(getServerMaxMoney(target));
+tprint(getServerGrowth(target));
+tprint(getServerRequiredHackingLevel(target));
