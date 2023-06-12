664f37c9361be030f4a4648651cff09419f932c7
Nolshine <mtr131@gmail.com>
2023-06-12 12:50:02 +0100

    expand server utility script

--- sv-util.ns.js
+++ sv-util.ns.js
@@ -5,6 +5,8 @@ export async function main(ns){
         "list": listPrices,
         "buy": tryBuy,
         "del": tryDel,
+        "poll": pollPservs,
+        "rn": renamePserv,
     };
 
     if (!ns.args[0] || !commands.hasOwnProperty(ns.args[0])){
@@ -17,25 +19,50 @@ export async function main(ns){
     return;
 }
 
+/** param {NS} ns */
+function renamePserv(ns, args){
+    let sv = args[0];
+    let name = args[1];
+    if (!ns.serverExists(sv)){
+        ns.tprint(`ERROR: ${sv} is not a valid server.`);
+    }
+    if (!ns.renamePurchasedServer(sv, name)){
+        ns.tprint(`ERROR: Could not rename ${sv} to ${name}.`);
+    }
+}
+
+function pollPservs (ns, args){
+    let prefix = 'pserv-';
+    for (let i = 0; i < 25; i++) {
+        let name = `${prefix}${i}`;
+        if (ns.serverExists(name)){
+            let ram = ns.getServerMaxRam(name);
+            ns.tprint(`${name}: ${ns.formatRam(ram)}`);
+        }
+    }
+}
+
 function printHelp(ns, args){
     ns.tprint("Current valid cli flags are:");
     ns.tprint("  help          - displays this helpfile.");
     ns.tprint("  list          - displays server prices per ram amount (in powers of 2)");
-    ns.tprint("  buy           - takes a num argument, attempts to buy a server.");
-    ns.tprint("  del           - takes a str argument, attempts to delete an existing server.");
+    ns.tprint("  buy           - takes an exponent (of 2) as an argument, attempts to buy a server.");
+    ns.tprint("  del           - takes a server name argument, attempts to delete an existing server.");
 }
 
 function listPrices(ns, args){
-    var formatter = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' });
     for (let exp = 1; exp <= 20; exp++) {
         let ram = Math.pow(2, exp);
-        let price = ns.getPurchasedServerCost(ram);
-        ns.tprint("A server with " + ram + "GB of RAM (2<sup>"+exp+"</sup>) costs "+formatter.format(price));
+        let f_ram = ns.formatRam(ram);
+        let price = ns.formatNumber(ns.getPurchasedServerCost(ram), 2);
+        ns.tprint(`A server with ${f_ram} of RAM (2^${exp}) costs \$${price}.`);
     }
 }
 
 function tryBuy(ns, args){
-    let ram = args[0];
+    let ram_factor = args[0];
+    let ram = Math.pow(2, ram_factor);
+    let f_ram = ns.formatRam(ram);
     if (!ram) {
         ns.tprint("ERROR: Required argument RAM not found.");
         return;
@@ -44,11 +71,11 @@ function tryBuy(ns, args){
         let sv = "pserv-" + i;
         if(!ns.serverExists(sv)){
             if (ns.getServerMoneyAvailable("home") < ns.getPurchasedServerCost(ram)){
-                ns.tprint("ERROR: Not enough money to buy server with " + ram + "GB RAM.");
+                ns.tprint("ERROR: Not enough money to buy server with " + f_ram + " RAM.");
                 return;
             }
             ns.purchaseServer(sv, ram);
-            ns.tprint("Bought server named: " + sv + " with " + ram + "GB RAM.");
+            ns.tprint("Bought server named: " + sv + " with " + f_ram + " RAM.");
             return;
         }
     }
